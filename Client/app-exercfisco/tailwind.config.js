ccafe931cad56018c50e9f363dccbe48822d699a
RicJM <116638687+RicJMC@users.noreply.github.com>
2024-06-26 12:26:56 +0100

    tentativa de colocar a funcionar
    tentei meter  a funcionar mas nao percebi o que era suposto fazer aqui

--- Client/app-exercfisco/tailwind.config.js
+++ Client/app-exercfisco/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Inclua todos os arquivos de conteúdo em `src`
+    './index.html',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
